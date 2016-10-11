083d916e4e8cd5cca7602d4b8a66cce8427f6d60
nadya <nadin.ts@gmail.com>
2016-10-11 22:34:27 +0300

    статистика для разных типов файлов

--- src/js/check.js
+++ src/js/check.js
@@ -1,44 +1,31 @@
 'use strict';
 
+window.getMessage = getMessage;
+
 function getMessage(a, b) {
-  var a,
-      b,
-      i;
-
-
-  if (typeof(a) === 'boolean') {
-
+  var i;
+  if ( typeof a === 'boolean') {
     if (a === true) {
       return ('Переданное GIF-изображение анимировано и содержит ' + b + 'кадров');
-    }
-    else {
+    } else {
       return ('Переданное GIF-изображение не анимировано');
     }
-
-  }
-
-  else if (typeof(a) === 'number') {
+  } else if (typeof a === 'number') {
     return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
-  }
-
-  else if (Array.isArray(a) === true) {
-
+  } else if (Array.isArray(a) === true) {
     if (Array.isArray(b) === true) {
       var artifactsSquare = 0;
-
       for (i = 0; i < a.length; i++) {
         artifactsSquare = artifactsSquare + a[i] * b[i];
       }
-      return ('Общая площадь артефактов сжатия: пикселей ' + artifactsSquare)
-    }
-    else {
+      return ('Общая площадь артефактов сжатия: пикселей ' + artifactsSquare);
+    } else {
       var amountOfRedPoints = 0;
-
       for (i = 0; i < a.length; i++) {
         amountOfRedPoints = amountOfRedPoints + a[i];
       }
-      return ('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints)
+      return ('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
     }
-
   }
+  return false;
 }
