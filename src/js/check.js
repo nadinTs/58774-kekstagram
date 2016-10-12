8c4de0f2edf2a17175aa0a62a65065e39944f086
nadya <nadin.ts@gmail.com>
2016-10-12 13:18:10 +0300

    bugfix

--- src/js/check.js
+++ src/js/check.js
@@ -6,7 +6,7 @@ function getMessage(a, b) {
   var i;
   if ( typeof a === 'boolean') {
     if (a === true) {
-      return ('Переданное GIF-изображение анимировано и содержит ' + b + 'кадров');
+      return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
     } else {
       return ('Переданное GIF-изображение не анимировано');
     }
@@ -14,11 +14,18 @@ function getMessage(a, b) {
     return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
   } else if (Array.isArray(a) === true) {
     if (Array.isArray(b) === true) {
-      var artifactsSquare = 0;
-      for (i = 0; i < a.length; i++) {
-        artifactsSquare = artifactsSquare + a[i] * b[i];
+      var rowsA = a.length,
+        rowsB = b.length,
+        artifactsSquare = 0;
+      if (rowsB === rowsA ) {
+        for (var j = 0; j < rowsA; j++) {
+          artifactsSquare += a[j] * b[j];
+          console.log(artifactsSquare);
+        }
+        return ('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
+      } else {
+        return false;
       }
-      return ('Общая площадь артефактов сжатия: пикселей ' + artifactsSquare);
     } else {
       var amountOfRedPoints = 0;
       for (i = 0; i < a.length; i++) {
