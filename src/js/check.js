694e216bdb07c145884adf6d54a70f5252cda75f
nadya <nadin.ts@gmail.com>
2016-10-17 20:12:44 +0300

    bugfix

--- src/js/check.js
+++ src/js/check.js
@@ -3,35 +3,33 @@
 window.getMessage = getMessage;
 
 function getMessage(a, b) {
-  var i;
   if ( typeof a === 'boolean') {
     if (a === true) {
       return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
     } else {
-      return ('Переданное GIF-изображение не анимировано');
+      return 'Переданное GIF-изображение не анимировано';
     }
   } else if (typeof a === 'number') {
-    return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
   } else if (Array.isArray(a) === true) {
     if (Array.isArray(b) === true) {
-      var rowsA = a.length,
-        rowsB = b.length,
-        artifactsSquare = 0;
-      if (rowsB === rowsA ) {
-        for (var j = 0; j < rowsA; j++) {
-          artifactsSquare += a[j] * b[j];
-          console.log(artifactsSquare);
-        }
-        return ('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
+      if (a.length === b.length ) {
+        var arr = a.map(function(value, index) {
+          return value * b[index];
+        });
+        var artifactsSquare = arr.reduce(function(x, y) {
+          return x + y;
+        });
+        return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
       } else {
         return false;
       }
     } else {
       var amountOfRedPoints = 0;
-      for (i = 0; i < a.length; i++) {
+      for (var i = 0; i < a.length; i++) {
         amountOfRedPoints = amountOfRedPoints + a[i];
       }
-      return ('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
+      return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
     }
   }
   return false;
